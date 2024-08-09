3c3ad6862d060a5b99872b88f14c1fddfde4265b
KevinJuela20 <fernando.dev2024@gmail.com>
2024-08-09 15:37:29 -0500

    Components card, experience and projects with trasnlation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
-import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), tailwind(), preact()]
+  integrations: [astroI18next(), tailwind()]
 });
